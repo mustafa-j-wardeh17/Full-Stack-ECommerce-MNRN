813eedba74d206b129807a82255a2b5df2859d1e
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-08 13:37:45 +0200

    create user schema

---
+++ backend/src/shared/schema/users.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export enum userTypes {
+    ADMIN = 'admin',
+    CUSTOMER = 'customer'
+}
+
+@Schema({
+    timestamps: true
+})
+export class User extends Document {
+    @Prop({ required: true })
+    name: string
+
+    @Prop({ required: true, unique: true })
+    email: string
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ required: true, enum: userTypes })
+    type: string;
+
+    @Prop({ default: false })
+    isVerified: boolean;
+
+    @Prop({ default: null })
+    otp: string;
+
+    @Prop({ default: null })
+    otpExpiryTime: Date;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
