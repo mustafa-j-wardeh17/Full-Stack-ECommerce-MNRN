bec779c47c9dec6627cb0678368a3ac2e8ed61b0
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-08 13:50:09 +0200

    set dto types for user resource

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { userTypes } from "src/shared/schema/users";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+
+    @IsNotEmpty()
+    @IsEnum(userTypes)
+    type: string;
+
+    @IsString()
+    @IsOptional()
+    secretToken?: string;
+}
--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    @IsString()
+    @IsOptional()
+    name?: string;
+
+    @IsString()
+    @IsOptional()
+    oldPassword?: string;
+
+    @IsString()
+    @IsOptional()
+    newPassword?: string;
+
+
+}
