a36b0fc34aac57ec587048759e540464ea740b4c
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-09 11:16:24 +0200

    create products resource and create a product schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import config from 'config'
 import { AllExceptionFilter } from './httpExceptionFilter';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 @Module({
   imports: [MongooseModule.forRoot(config.get('mongodbUrl'), {
     w: 1,
-  }), UsersModule],
+  }), UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService,
     {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from 'config'
 import { TransformationInterception } from './responseInterceptor';
+import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser()) // to apply working with cookies
   app.useGlobalInterceptors(new TransformationInterception())
   await app.listen(config.get('port'));
 }
