7464db540f656f394c5843f2fb96012dad9dc06f
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-10 13:12:41 +0200

    set up order module successfully by applying App Guard and import stripe and define needed schemas

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,9 +1,44 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from 'src/shared/repositories/order.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Orders, OrderSchema } from 'src/shared/schema/order';
+import { AuthMiddleware } from 'src/middleware/auth';
+import config from 'config'
+import { Products, ProductSchema } from 'src/shared/schema/products';
+import { License, LicenseSchema } from 'src/shared/schema/license';
+import { Users, UserSchema } from 'src/shared/schema/users';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from 'src/middleware/roles.guard';
+import { StripeModule } from '@golevelup/nestjs-stripe';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository, {
+    provide: APP_GUARD,
+    useClass: RolesGuard
+  }],
+  imports: [
+    StripeModule.forRoot(StripeModule, {
+      apiKey: config.get('stripe.secret_key'),
+    }),
+    MongooseModule.forFeature([
+      { name: Products.name, schema: ProductSchema },
+      { name: License.name, schema: LicenseSchema },
+      { name: Users.name, schema: UserSchema },
+      { name: Orders.name, schema: OrderSchema }
+    ])
+  ]
 })
-export class OrdersModule {}
+export class OrdersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({
+        path: `${config.get('appPrefix')}/orders/webhook`,
+        method: RequestMethod.POST,
+      })
+      .forRoutes(OrdersController);
+  }
+}
