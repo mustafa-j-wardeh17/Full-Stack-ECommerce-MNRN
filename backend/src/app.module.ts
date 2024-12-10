74b4d35a922bbce75a1e65093e98dfb721391e51
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-10 12:58:15 +0200

    create orders resource and create a orders schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,11 +6,12 @@ import config from 'config'
 import { AllExceptionFilter } from './httpExceptionFilter';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 @Module({
   imports: [MongooseModule.forRoot(config.get('mongodbUrl'), {
     w: 1,
     
-  }), UsersModule, ProductsModule],
+  }), UsersModule, ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService,
     {
