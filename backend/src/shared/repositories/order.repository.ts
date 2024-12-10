9d9bf7a21694aa4a4500f96c8d3f1e035f06d69d
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-10 13:04:18 +0200

    initialize oredersRepository

---
+++ backend/src/shared/repositories/order.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Orders } from "../schema/order";
+import { Document, Model } from "mongoose";
+
+
+@Injectable()
+export class OrdersRepository {
+    constructor(
+        @InjectModel(Orders.name) private readonly orderModel: Model<Orders & Document>,
+    ) { }
+}
