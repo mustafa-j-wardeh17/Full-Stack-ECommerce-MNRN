b735cffeee82b5be6cc362f46cb0a780cbb9a9b9
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-15 22:23:06 +0200

    #

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default async function Home() {
   const response = await fetch('https://mnrn-shop-backend.onrender.com/api/v1')
   const result = await response.json()
