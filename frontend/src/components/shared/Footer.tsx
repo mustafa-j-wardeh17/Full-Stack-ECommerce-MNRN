e1f980922b766dd3318332069a49e58e1976dade
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-17 18:04:53 +0200

    Enhance footer

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
+import { ArrowRightIcon } from 'lucide-react';
+import { Separator } from '@radix-ui/react-dropdown-menu';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -15,17 +18,7 @@ const Footer = () => {
                         <li><a href="mailto:psstore@example.com" className="hover:underline transition duration-300 ease-in-out"><i className="fas fa-envelope mr-2"></i> krist@example.com</a></li>
                         <li><a href="#" className="hover:underline transition duration-300 ease-in-out"><i className="fas fa-map-marker-alt mr-2"></i> 3891 CA 62639</a></li>
                     </ul>
-                    <div className="flex space-x-4 mt-4">
-                        <div className="w-12 h-auto py-1 bg-secondary flex items-center justify-center rounded-sm">
-                            <Image src="/visa.svg" alt="Visa" width={26} height={18} />
-                        </div>
-                        <div className="w-12 h-auto py-1 bg-secondary flex items-center justify-center rounded-sm">
-                            <Image src="/mastercard.svg" alt="Mastercard" width={26} height={18} />
-                        </div>
-                        <div className="w-12 h-auto py-1 bg-secondary flex items-center justify-center rounded-sm">
-                            <Image src="/paypal.svg" alt="Paypal" width={26} height={18} />
-                        </div>
-                    </div>
+
                 </div>
 
                 {/* Information Links */}
@@ -53,22 +46,42 @@ const Footer = () => {
                 </div>
 
                 {/* Subscribe Section */}
-                <div className="flex w-full max-w-[300px] flex-col space-y-6 text-sm">
+                <div className="flex w-full max-w-[300px] flex-col space-y-4 text-sm">
                     <h3 className="text-lg font-semibold">Subscribe</h3>
-                    <div className="flex flex-col space-y-4">
-                        <input type="email" placeholder="Your Email" className="p-3 rounded-lg border border-gray-600 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300" />
-                        <button className="bg-blue-600 text-white py-2 px-6 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out">Subscribe</button>
+                    <p className='text-xs text-primary/70'>
+                        Entre your email below to be the rirst to know about new collections and products launches.
+                    </p>
+                    <div className="flex flex-col  relative">
+                        <input type="email" placeholder="Your Email" className="p-3 rounded-lg border border-primary bg-primary-foreground text-primary focus:outline-none focus:ring-2 focus:ring-secondary transition duration-300" />
+                        <button className="absolute right-2 top-[50%] -translate-y-[50%]  transition duration-300 ease-in-out">
+                            <ArrowRightIcon size={18} />
+                        </button>
                     </div>
                 </div>
             </div>
 
             {/* Footer Bottom */}
-            <div className="text-center mt-12">
-                <p className="text-sm">©2024 PS_Store All Rights Reserved</p>
-                <div className="flex justify-center space-x-6 mt-4">
-                    <a href="#" className="hover:underline transition duration-300 ease-in-out">Facebook</a>
-                    <a href="#" className="hover:underline transition duration-300 ease-in-out">Instagram</a>
-                    <a href="#" className="hover:underline transition duration-300 ease-in-out">Twitter</a>
+            <Separator color='white' />
+            <div className="mx-auto w-full px-6 md:px-12 flex flex-col gap-6  mt-12">
+                <div className='w-full h-[1px] bg-primary/40'/>
+                <div className='flex items-center w-full  justify-between'>
+                    <div className="sm:flex hidden space-x-4 ">
+                        <div className="w-10 h-[30px] py-1 bg-primary flex items-center justify-center rounded-sm">
+                            <Image src="/visa.svg" alt="Visa" width={24} height={12} />
+                        </div>
+                        <div className="w-10 h-[30px] py-1 bg-primary flex items-center justify-center rounded-sm">
+                            <Image src="/mastercard.svg" alt="Mastercard" width={20} height={12} />
+                        </div>
+                        <div className="w-10 h-[30px] py-1 bg-primary flex items-center justify-center rounded-sm">
+                            <Image src="/paypal.svg" alt="Paypal" width={20} height={12} />
+                        </div>
+                    </div>
+                    <p className="text-sm">©2024 PS_Store All Rights Reserved</p>
+                    <div className="flex justify-center space-x-2 text-primary">
+                        <a href="#"><FaFacebook className='hover:scale-110 hover:text-primary/80' /></a>
+                        <a href="#"><FaInstagram className='hover:scale-110 hover:text-primary/80' /></a>
+                        <a href="#"><FaTwitter className='hover:scale-110 hover:text-primary/80' /></a>
+                    </div>
                 </div>
             </div>
         </footer>
