fec1d56b97f916a6e159b80890f799528900253a
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-12-22 21:58:20 +0200

    feat: implement alert dialog for product deletion confirmation and add Radix UI dependency

--- frontend/src/components/ProductCardDeleteButton.tsx
+++ frontend/src/components/ProductCardDeleteButton.tsx
@@ -2,10 +2,22 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useRouter } from "next/navigation";
 
 const DeleteProductButton = ({ productId }: { productId: string }) => {
     const [isDeleting, setIsDeleting] = useState<boolean>(false);
-
+    const router = useRouter()
     const handleDeleteProduct = async () => {
         setIsDeleting(true);
         try {
@@ -19,7 +31,9 @@ const DeleteProductButton = ({ productId }: { productId: string }) => {
             if (!response.ok) {
                 throw new Error(`Failed to delete product: ${response.statusText}`);
             }
+
             toast.success('Product deleted successfully');
+            router.refresh();
         } catch (error: any) {
             toast.error(error.message || 'Failed to delete product. Please try again later.');
         } finally {
@@ -29,13 +43,33 @@ const DeleteProductButton = ({ productId }: { productId: string }) => {
 
     return (
         <>
-            <button
-                className="p-2 bg-white rounded-full shadow-md hover:shadow-lg flex items-center justify-center"
-                onClick={handleDeleteProduct}
-                disabled={isDeleting}
-            >
-                <RiDeleteBin6Line className="text-red-500" size={18} />
-            </button>
+
+
+            <AlertDialog>
+                <AlertDialogTrigger
+                    className="p-2 bg-white rounded-full shadow-md hover:shadow-lg flex items-center justify-center"
+                >
+                    <RiDeleteBin6Line className="text-red-500" size={18} />
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                            onClick={handleDeleteProduct}
+                            color="red"
+                        >
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </>
     );
 }
