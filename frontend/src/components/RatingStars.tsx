85a83166c4da14f1cacaad95583f4fdf6f1ad71b
mustafawardeh <mostafa.wardeh2000@gmail.com>
2025-01-10 19:36:13 +0200

    #

--- frontend/src/components/RatingStars.tsx
+++ frontend/src/components/RatingStars.tsx
@@ -1,21 +1,24 @@
-'use client'
+'use client';
 import React, { memo } from 'react';
 import { FaStar } from 'react-icons/fa';
 
-const RatingStars = memo(({ avgRating }:{avgRating:number}) => {
-  const filledStars = Math.floor(avgRating || 0);  // Pre-calculate the number of filled stars
+const RatingStars = memo(({ avgRating }: { avgRating: number }) => {
+  const filledStars = Math.floor(avgRating || 0); // Pre-calculate the number of filled stars
   const stars = Array(5).fill(false).map((_, idx) => idx < filledStars);
 
   return (
     <span className="flex xl:text-sm text-xs space-x-1 items-center">
       {stars.map((isFilled, idx) => (
         <FaStar
-          key={idx} // Use index for key (fine in this case as it’s static)
-          className={isFilled ? 'text-amber-500' : 'text-gray-300'}
+          key={idx} 
+          className={isFilled ? 'text-amber-500' : 'text-primary/30'}
         />
       ))}
     </span>
   );
 });
+
+// Set the displayName for the component
+RatingStars.displayName = 'RatingStars';
 
 export default RatingStars;
